3e5f7dc99a0ff42c78c2ec2e456d174586d61cfd
Kris Petrič <77499501+krisp3t@users.noreply.github.com>
2025-02-08 13:46:17 +0100

    Initial commit

---
+++ Logger.h
@@ -0,0 +1,28 @@
+#pragma once
+
+namespace KrisLogger
+{
+
+#define LOGI(...) KrisLogger::Logger::Log(KrisLogger::LogLevel::LOG_INFO, __FILE__, __LINE__, __VA_ARGS__)
+#define LOGD(...) KrisLogger::Logger::Log(KrisLogger::LogLevel::LOG_DEBUG, __FILE__, __LINE__, __VA_ARGS__)
+#define LOGW(...) KrisLogger::Logger::Log(KrisLogger::LogLevel::LOG_WARN, __FILE__, __LINE__, __VA_ARGS__)
+#define LOGE(...) KrisLogger::Logger::Log(KrisLogger::LogLevel::LOG_ERROR, __FILE__, __LINE__, __VA_ARGS__)
+
+    enum class LogLevel
+    {
+        LOG_DEBUG,
+        LOG_INFO,
+        LOG_WARN,
+        LOG_ERROR
+    };
+
+    class Logger
+    {
+    public:
+        static void Log(LogLevel level, const char *file, int line, const char *format, ...);
+        static void SetLogLevel(LogLevel level);
+    private:
+        static LogLevel _minLevel;
+    };
+
+} // namespace KrisLogger
